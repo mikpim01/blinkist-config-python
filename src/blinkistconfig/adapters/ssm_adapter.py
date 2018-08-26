ba0bfbd523e802462f36154afe7eb7228e10e34a
Peter Shoukry <pshoukry@gmail.com>
2018-08-26 13:10:32 +0200

    Add SSM adapter

--- src/blinkistconfig/adapters/ssm_adapter.py
+++ src/blinkistconfig/adapters/ssm_adapter.py
@@ -7,4 +7,6 @@ DEFAULT_CLIENT = boto3.client('ssm', region_name=REGION)
 
 class SSMAdapter():
     def get(self, key, scope=None, app_name=None, client=DEFAULT_CLIENT):
-        return client.get_parameter(Name=key, WithDecryption=True)
+        prefix = "/application"
+        key_scope = scope if scope else app_name
+        return client.get_parameter(Name=f"{prefix}/{key_scope}/{key}", WithDecryption=True)
--- test/blinkistconfig/adapters/test_ssm_adapter.py
+++ test/blinkistconfig/adapters/test_ssm_adapter.py
@@ -1,5 +1,19 @@
-import blinkistconfig.adapters
+import pytest
+
+@pytest.fixture
+def ssm_client():
+    import boto3
+    return boto3.client('ssm', region_name='us-east-1')
+
+@pytest.fixture
+def ssm_adapter():
+    import blinkistconfig.adapters
+    return blinkistconfig.adapters.SSMAdapter()
+
+def test_ssm_adapter_get_reads_variable_from_app_scope(ssm_client, ssm_adapter, mocker):
+    get_param_stub = mocker.patch.object(ssm_client, 'get_parameter')
+    ssm_adapter.get('a_key',app_name="an_app", client=ssm_client)
+    get_param_stub.assert_called_once_with(Name=f"/application/an_app/a_key", WithDecryption=True)
 
 
-def test_env_adapter_get_reads_environment_variable(mocker):
-    pass
+
