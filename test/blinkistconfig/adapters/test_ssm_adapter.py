969d4e226adb336fb1414d916e306275516a653b
Peter Shoukry <pshoukry@gmail.com>
2018-08-26 13:13:25 +0200

    Handle forced scope if passed

--- test/blinkistconfig/adapters/test_ssm_adapter.py
+++ test/blinkistconfig/adapters/test_ssm_adapter.py
@@ -15,5 +15,9 @@ def test_ssm_adapter_get_reads_variable_from_app_scope(ssm_client, ssm_adapter, 
     ssm_adapter.get('a_key',app_name="an_app", client=ssm_client)
     get_param_stub.assert_called_once_with(Name=f"/application/an_app/a_key", WithDecryption=True)
 
+def test_ssm_adapter_get_reads_variable_from_scope_if_scope_forced(ssm_client, ssm_adapter, mocker):
+    get_param_stub = mocker.patch.object(ssm_client, 'get_parameter')
+    ssm_adapter.get('a_key',app_name="an_app", client=ssm_client, scope='global')
+    get_param_stub.assert_called_once_with(Name=f"/application/global/a_key", WithDecryption=True)
 
 
