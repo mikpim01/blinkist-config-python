62bec93329150d79be7eac5bff68c548c89bf461
Peter Shoukry <pshoukry@gmail.com>
2018-08-27 21:09:54 +0200

    Handle key now found in ssm

--- src/blinkistconfig/adapters/ssm_adapter.py
+++ src/blinkistconfig/adapters/ssm_adapter.py
@@ -8,4 +8,8 @@ class SSMAdapter():
     def get(self, key, scope=None, app_name=None, client=DEFAULT_CLIENT):
         prefix = "/application"
         key_scope = scope if scope else app_name
-        return client.get_parameter(Name=f"{prefix}/{key_scope}/{key}", WithDecryption=True)
+        try:
+            response = client.get_parameter(Name=f"{prefix}/{key_scope}/{key}", WithDecryption=True)
+            return response['Value']
+        except client.exceptions.ParameterNotFound:
+            return None
