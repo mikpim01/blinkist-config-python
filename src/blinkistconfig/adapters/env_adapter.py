b78abc7659f51e583b6d08e08bb59d61fda522e5
Peter Shoukry <pshoukry@gmail.com>
2018-08-27 11:14:27 +0200

    Use native method

--- src/blinkistconfig/adapters/env_adapter.py
+++ src/blinkistconfig/adapters/env_adapter.py
@@ -3,7 +3,4 @@ import os
 class ENVAdapter():
     def get(self, key, scope=None, app_name=None, client=None):
         key = key.replace("/", "_").upper()
-        try:
-            return os.environ[key]
-        except:
-            return None
+        return os.environ.get(key)
