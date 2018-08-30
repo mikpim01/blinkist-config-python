1b1a4a6dd92599ea275a6156e2c4616e023f0cad
Peter Shoukry <pshoukry@gmail.com>
2018-08-30 14:28:12 +0200

    Better test coverage

--- test/blinkistconfig/test_config.py
+++ test/blinkistconfig/test_config.py
@@ -1,6 +1,9 @@
 import pytest
-import blinkistconfig.adapters
 from pytest_mock import mocker
+
+import blinkistconfig.adapters
+
+import os
 
 @pytest.fixture
 def config_class():
@@ -15,3 +18,11 @@ def test_config_get_raises_argument_error_with_more_than_one_default(config_clas
 def test_config_infers_the_adapter_from_adapter_type(config_class, mocker):
     assert config_class._adapter().__class__ == blinkistconfig.adapters.ENVAdapter
     assert config_class.adapter.__class__ == blinkistconfig.adapters.ENVAdapter
+
+def test_config_get_returns_default_if_key_does_not_exist(config_class, mocker):
+    assert config_class.get("missing_key", "1234") == "1234"
+
+def test_conf_get_returns_adapter_value_if_key_exist(config_class, mocker):
+    mocker.patch.dict(os.environ, {"EXISTING_KEY": "12345"})
+    assert config_class.get("existing_key", "1234") == "12345"
+
